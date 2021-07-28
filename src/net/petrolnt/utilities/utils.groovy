f77c1a1eb76732ee1ab0aa01d8c4fde7405e64b1
Petr Konev <petr_konev@epam.com>
2021-07-28 18:52:44 +0500

    test

--- src/net/petrolnt/utilities/utils.groovy
+++ src/net/petrolnt/utilities/utils.groovy
@@ -1,6 +1,6 @@
 package net.petrolnt.jpipelines
 
-class Utilities implements Serializable {
+class utils implements Serializable {
   def steps
   Utilities(steps) {this.steps = steps}
   static def checkOutFrom(repo) {
