9fbe1d1d104c47ec4e3365c9a33807af4c91517c
Petr Konev <petr_konev@epam.com>
2021-07-29 10:38:43 +0500

    test

--- src/net/petrolnt/utilities/Utilities.groovy
+++ src/net/petrolnt/utilities/Utilities.groovy
@@ -2,6 +2,6 @@ package net.petrolnt.utilities
 class Utilities implements Serializable {
 
   static def checkOutFrom(branchesList, credentials, repo) {
-    git([branches: branchesList, credentialsId: credentials, url: repo)
+    git([branches: branchesList, credentialsId: credentials, url: repo])
   }
 }
