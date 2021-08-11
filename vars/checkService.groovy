ad57ce6197bad13f88da2b77bc8ac2f37bb81452
Petr Konev <petr_konev@epam.com>
2021-08-11 09:28:00 +0500

    Check status script

--- vars/checkService.groovy
+++ vars/checkService.groovy
@@ -1,14 +1,25 @@
 def call(body) {
-    // GET
+    // Check service
     def get = new URL("http://192.168.1.60").openConnection();
     def getRC = get.getResponseCode();
-    println(getRC);
     if (getRC.equals(200)) {
-        echo "Service is running"
+        println("Service is running")
         currentBuild.result = 'SUCCESS' 
     }
     else {
-        echo "Service not started"
+        println("Service not started")
+        currentBuild.result = 'FAILURE' 
+    }
+    // Check service health
+    def get = new URL("http://192.168.1.60/health").openConnection();
+    def getRC = get.getResponseCode();
+    if (getRC.equals(200)) {
+        println("Service status is healthy")
+        currentBuild.result = 'SUCCESS' 
+    }
+    else {
+        println("Service status is unhealthy")
+        println("Responce code is: " + getRC)
         currentBuild.result = 'FAILURE' 
     }
     return this
